0939924f7b2e7c78cd30b928419dd3ee34aac26d
Javohir-star <jmamadiyorov24@gmail.com>
2026-05-05 16:52:52 +0500

    add: solve

--- accounts/views/pagenation.py
+++ accounts/views/pagenation.py
@@ -4,11 +4,11 @@ from accounts.serializers import UserSerializer, BookSerializer
 from accounts.pagenation import CustomPagination 
 
 class UserListView(ListAPIView):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
     pagination_class = CustomPagination
 
 class BookListView(ListAPIView):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().order_by('id')
     serializer_class = BookSerializer
     pagination_class = CustomPagination
--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
 
 SPECTACULAR_SETTINGS = {
